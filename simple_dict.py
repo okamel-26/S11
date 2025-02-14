79a6f9b66a1a1fb1997284fd9899679ed1b6fabb
Omar Kamel <okamel.ieu2023@student.ie.edu>
2025-02-14 12:48:47 +0100

    added simple dictionary

---
+++ simple_dict.py
@@ -0,0 +1,6 @@
+d = {} # empty dict
+eng_to_spa = {"one": "uno", "two": "dos", "three": "tres", "pineapple": "pina"}
+print(eng_to_spa)
+print(eng_to_spa["three"])
+print(eng_to_spa["pineapple"]) == "pina" # you can add to the dict
+print(eng_to_spa["pineapple"])
